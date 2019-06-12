101cb3466add5e6abb8e55b8c442562225fe9ffc
Amanda E Nelson <amanda.nelson@yahoo.com>
2019-06-12 21:12:08 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,7 +7,7 @@ def valid_move?(board, index)
 
   def position_taken?(board, index)
     if board[index] == " " || board[index] == "" || board[index] == nil
-      false
+      true
     elsif board[index] == "X" || board[index] == "O"
       true
     end
